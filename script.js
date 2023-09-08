d9acb09e1dbf3979af4426a153a5416c8031571a
tbagaciunas <113136639+tbagaciunas@users.noreply.github.com>
2023-09-08 10:58:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const skillsProg = document.querySelectorAll('.skills-prog li[data-percent]');
-    const skillsSoft = document.querySelectorAll('.skills-soft li[data-percent]');
-
-    skillsProg.forEach(skill => {
-        const percent = skill.getAttribute('data-percent');
-        const progressBar = skill.querySelector('.skills-bar .bar');
-        progressBar.style.width = percent + '%';
-    });
-
-    skillsSoft.forEach(skill => {
-        const percent = skill.getAttribute('data-percent');
-        const progressBar = skill.querySelector('.cbar');
-        progressBar.style.strokeDashoffset = 283 - (percent / 100) * 283;
-    });
-});
+$('.skills-prog li').find('.skills-bar').each (i) ->
+  $(this).find('.bar').delay(i*150).animate {
+    width: $(this).parents().attr('data-percent') + '%'
+  }, 1000, 'linear', ->
+    $(this).css 'transition-duration': '.5s'
+  return
+$('.skills-soft li').find('svg').each (i) ->
+  circle = $(this).children('.cbar')
+  r = circle.attr('r')
+  c = Math.PI * (r * 2)
+  percent = $(this).parent().data 'percent'
+  cbar = ((100-percent)/100) * c
+  circle.css 'stroke-dashoffset': c, 'stroke-dasharray': c
+  circle.delay(i*150).animate {
+    strokeDashoffset: cbar
+  }, 1000, 'linear', ->
+    circle.css 'transition-duration': '.3s'
+  $(this).siblings('small').prop('Counter', 0).delay(i*150).animate {
+    Counter: percent
+  }, duration: 1000, step: (now) ->
+    $(this).text Math.ceil(now) + '%'
+  return
