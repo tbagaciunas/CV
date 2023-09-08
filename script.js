025f51b05a17d3debdc22db93b2f342f444c5f7f
tbagaciunas <113136639+tbagaciunas@users.noreply.github.com>
2023-09-08 11:01:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,34 @@
-$('.skills-prog li').find('.skills-bar').each (i) ->
-  $(this).find('.bar').delay(i*150).animate {
-    width: $(this).parents().attr('data-percent') + '%'
-  }, 1000, 'linear', ->
-    $(this).css 'transition-duration': '.5s'
-  return
-$('.skills-soft li').find('svg').each (i) ->
-  circle = $(this).children('.cbar')
-  r = circle.attr('r')
-  c = Math.PI * (r * 2)
-  percent = $(this).parent().data 'percent'
-  cbar = ((100-percent)/100) * c
-  circle.css 'stroke-dashoffset': c, 'stroke-dasharray': c
-  circle.delay(i*150).animate {
-    strokeDashoffset: cbar
-  }, 1000, 'linear', ->
-    circle.css 'transition-duration': '.3s'
-  $(this).siblings('small').prop('Counter', 0).delay(i*150).animate {
-    Counter: percent
-  }, duration: 1000, step: (now) ->
-    $(this).text Math.ceil(now) + '%'
-  return
+$(document).ready(function () {
+  $('.skills-prog li').find('.skills-bar').each(function (i) {
+    var $bar = $(this).find('.bar');
+    var percent = $bar.parent().data('percent') + '%';
+
+    $bar.delay(i * 150).animate({ width: percent }, 1000, 'linear', function () {
+      $bar.css('transition-duration', '.5s');
+    });
+  });
+
+  $('.skills-soft li').find('svg').each(function (i) {
+    var $circle = $(this).children('.cbar');
+    var r = $circle.attr('r');
+    var c = Math.PI * (r * 2);
+    var percent = $circle.parent().data('percent');
+    var cbar = ((100 - percent) / 100) * c;
+
+    $circle.css({ 'stroke-dashoffset': c, 'stroke-dasharray': c });
+
+    $circle.delay(i * 150).animate({ strokeDashoffset: cbar }, 1000, 'linear', function () {
+      $circle.css('transition-duration', '.3s');
+    });
+
+    $(this).siblings('small').prop('Counter', 0).delay(i * 150).animate(
+      { Counter: percent },
+      {
+        duration: 1000,
+        step: function (now) {
+          $(this).text(Math.ceil(now) + '%');
+        },
+      }
+    );
+  });
+});
