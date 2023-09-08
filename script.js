cf869574874494c38269ce1214773d09f625443f
tbagaciunas <113136639+tbagaciunas@users.noreply.github.com>
2023-09-08 11:41:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,138 @@
-$(document).ready(function () {
-  $('.skills-prog li').find('.skills-bar').each(function (i) {
-    var $bar = $(this).find('.bar');
-    var percent = $bar.parent().data('percent') + '%';
+/*
+Please do not claim this website as your own. You don't have to give me credit but DON'T claim it's your code. When you use this as a base for your website please remove all tracking (Google Analytics, Heap).
+*/
 
-    $bar.delay(i * 150).animate({ width: percent }, 1000, 'linear', function () {
-      $bar.css('transition-duration', '.5s');
-    });
-  });
+// PreLoader
+jQuery.noConflict();
+(function($) {
+	$(window).on('load', function() { // makes sure the whole site is loaded
+		$('#status').fadeOut(); // will first fade out the loading animation
+		$('#preloader').delay(200).fadeOut('slow'); // will fade out the white DIV that covers the website.
+	});
+})(jQuery);
 
-  $('.skills-soft li').find('svg').each(function (i) {
-    var $circle = $(this).children('.cbar');
-    var r = $circle.attr('r');
-    var c = Math.PI * (r * 2);
-    var percent = $circle.parent().data('percent');
-    var cbar = ((100 - percent) / 100) * c;
+// Scroll to Top
+jQuery.noConflict();
+(function($) {
+	$(window).scroll(function() {
+		if ($(this).scrollTop() >= 50) { // If page is scrolled more than 50px
+			$('#return-to-top').fadeIn(200); // Fade in the arrow
+		} else {
+			$('#return-to-top').fadeOut(200); // Else fade out the arrow
+		}
+	});
+	$('#return-to-top').click(function() { // When arrow is clicked
+		$('body,html').animate({
+			scrollTop: 0 // Scroll to top of body
+		}, 500);
+	});
+})(jQuery);
 
-    $circle.css({ 'stroke-dashoffset': c, 'stroke-dasharray': c });
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+jQuery.noConflict();
+(function($) {
+	$(function() {
+		$('a.page-scroll').bind('click', function(event) {
+			var $anchor = $(this);
+			$('html, body').stop().animate({
+				scrollTop: $($anchor.attr('href')).offset().top
+			}, 1500, 'easeInOutExpo');
+			event.preventDefault();
+		});
+	});
+})(jQuery);
 
-    $circle.delay(i * 150).animate({ strokeDashoffset: cbar }, 1000, 'linear', function () {
-      $circle.css('transition-duration', '.3s');
-    });
+// typer for hello
+window.onload = function() {
+	var elements = document.getElementsByClassName('txt-rotate');
+	for (var i = 0; i < elements.length; i++) {
+		var toRotate = elements[i].getAttribute('data-rotate');
+		var period = elements[i].getAttribute('data-period');
+		if (toRotate) {
+			new TxtRotate(elements[i], JSON.parse(toRotate), period);
+		}
+	}
+	// INJECT CSS
+	var css = document.createElement("style");
+	css.type = "text/css";
+	css.innerHTML = ".txt-rotate > .wrap { border-right: 10px solid #40E0D0 }";
+	document.body.appendChild(css);
+};
 
-    $(this).siblings('small').prop('Counter', 0).delay(i * 150).animate(
-      { Counter: percent },
-      {
-        duration: 1000,
-        step: function (now) {
-          $(this).text(Math.ceil(now) + '%');
-        },
-      }
-    );
-  });
-});
+var TxtRotate = function(el, toRotate, period) {
+	this.toRotate = toRotate;
+	this.el = el;
+	this.loopNum = 0;
+	this.period = parseInt(period, 1) || 1000;
+	this.txt = '';
+	this.tick();
+	this.isDeleting = false;
+};
+
+TxtRotate.prototype.tick = function() {
+	var i = this.loopNum % this.toRotate.length;
+	var fullTxt = this.toRotate[i];
+
+	if (this.isDeleting) {
+		this.txt = fullTxt.substring(0, this.txt.length - 1);
+	} else {
+		this.txt = fullTxt.substring(0, this.txt.length + 1);
+	}
+
+	this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
+
+	var that = this;
+	var delta = 200 - Math.random() * 100;
+
+	if (this.isDeleting) {
+		delta /= 2;
+	}
+
+	if (!this.isDeleting && this.txt === fullTxt) {
+		delta = this.period;
+		this.isDeleting = true;
+	} else if (this.isDeleting && this.txt === '') {
+		this.isDeleting = false;
+		this.loopNum++;
+		delta = 200;
+	}
+
+	setTimeout(function() {
+		that.tick();
+	}, delta);
+};
+
+// number count for stats
+jQuery.noConflict();
+(function($) {
+	$('.counter').each(function() {
+		var $this = $(this),
+			countTo = $this.attr('data-count');
+
+		$({
+			countNum: $this.text()
+		}).animate({
+				countNum: countTo
+			},
+
+			{
+				duration: 3000,
+				easing: 'linear',
+				step: function() {
+					$this.text(Math.floor(this.countNum));
+				},
+				complete: function() {
+					$this.text(this.countNum);
+					//alert('finished');
+				}
+			});
+	});
+})(jQuery);
+
+// update footer copyright year
+
+var today = new Date();
+var year = today.getFullYear();
+
+var copyright = document.getElementById("copyright");
+copyright.innerHTML = '© Marina Marques '+ year;
